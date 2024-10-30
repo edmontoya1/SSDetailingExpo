c34da913cbbb0dfc4e9cdd5319c8bd6d005bd361
Eduardo Montoya <montoyaeduardo2015@gmail.com>
2024-10-29 22:24:30 -0500

    update home page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,18 @@
 import { StyleSheet } from "react-native";
 
-import EditScreenInfo from "@/components/EditScreenInfo";
 import { Text, View } from "@/components/Themed";
+import Colors from "@/constants/Colors";
 
 export default function home() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Home</Text>
+      <Text style={styles.description}>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam,
+        voluptates temporibus impedit magnam voluptas optio? Natus quo ipsam sit
+        accusantium a repellat suscipit eos, numquam sapiente sed blanditiis
+        quis fugiat.
+      </Text>
     </View>
   );
 }
@@ -18,8 +24,12 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   title: {
-    fontSize: 20,
-    fontWeight: "bold",
+    fontSize: 100,
+    fontWeight: "900",
+    color: Colors.green.primary.color,
+  },
+  description: {
+    color: Colors.yellow.primary.color,
   },
   separator: {
     marginVertical: 30,
