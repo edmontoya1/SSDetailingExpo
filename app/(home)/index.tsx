9de3ac7ba2379095fe76ce4f5f9283c4ae6145cb
Eduardo Montoya <montoyaeduardo2015@gmail.com>
2024-10-29 21:50:12 -0500

    added sign out functionality

--- app/(home)/index.tsx
+++ app/(home)/index.tsx
@@ -1,14 +1,26 @@
-import { SignedIn, SignedOut, useUser } from "@clerk/clerk-expo";
+import { SignedIn, SignedOut, useAuth, useUser } from "@clerk/clerk-expo";
 import { Link } from "expo-router";
-import { SafeAreaView, Text, View } from "react-native";
+import { Pressable, SafeAreaView, Text, View } from "react-native";
 
 export default function Page() {
   const { user } = useUser();
+  const { signOut } = useAuth();
+
+  const handleSignOut = async () => {
+    try {
+      await signOut();
+    } catch (error) {
+      console.error("Error signing out: ", error);
+    }
+  };
 
   return (
     <SafeAreaView>
       <SignedIn>
         <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
+        <Pressable onPress={handleSignOut}>
+          <Text>Sign Out</Text>
+        </Pressable>
       </SignedIn>
       <SignedOut>
         <Link href="/sign-in">
