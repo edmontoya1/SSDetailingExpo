911de3d5f0dbf31e6ded2bca48ed326a493af95f
Eduardo Montoya <montoyaeduardo2015@gmail.com>
2024-10-29 19:14:58 -0500

    Initial Commit

---
+++ app/(auth)/_layout.tsx
@@ -0,0 +1,19 @@
+import { Redirect, Stack } from "expo-router";
+import { useAuth } from "@clerk/clerk-expo";
+
+export default function AuthRoutesLayout() {
+  const { isSignedIn } = useAuth();
+
+  if (isSignedIn) {
+    return <Redirect href={"/(home)/"} />;
+  }
+
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: false,
+        gestureEnabled: false,
+      }}
+    />
+  );
+}
